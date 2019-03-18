b8dcc6377f1fa1810c8a8826b7796f9ba4986d28
j.sarathkrishnaswamy <sarathkrishnaswamy.95@gmail.com>
2019-03-18 16:31:21 +0530

    Initial Commit

---
+++ AppCorethings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCorethings
+//
+//  Created by J.sarath Krishnaswamy on 18/03/19.
+//  Copyright © 2019 J.sarath Krishnaswamy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
